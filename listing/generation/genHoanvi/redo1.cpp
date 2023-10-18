b4457fbe16dfee3293e5305ced8e69264cfe9fc2
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2023-10-18 20:15:04 +0700

    redo1 part 2

--- listing/generation/genHoanvi/redo1.cpp
+++ listing/generation/genHoanvi/redo1.cpp
@@ -1,35 +1,58 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 int n;
 int x[100];
 void display() {
     for (int i = 1; i <= n; i++) {
-        cout << x[i] ;
+        cout << x[i] << " ";
     }
     cout << endl;
 }
 bool isFinal( ) {
-    for (int i = n - 1; i >= 0; ) {
-        if (x[i] != n - k + i) {
+    for (int i = 1; i <= n; i++) {
+        if (x[i] != (n - i + 1)) {
             return false;
         }
     }
     return true;
 }
 void init() {
-    for (int i = 1; i <= k; i++) {
+    for (int i = 1; i <= n; i++) {
         x[i] = i;
     }
 }
 void genNext( ){
-    int t = k;
-    while (x[t] == (n - k + t)) {
+    int t = n;
+    int temp = x[n];
+    t--;
+    while (x[t] > temp) {
+        temp = x[t];
         t--;
     }
-    x[t]++;
-    for (int i = t + 1; i <= k; i++) {
-        x[i] = x[i - 1] + 1;
+    int max_index = t + 1;
+    int min_sub = x[t + 1] - x[t];
+    for (int i = t + 2;  i <= n; i++) {
+        int sub = x[i] - x[t];
+        if (sub > 0 && sub < min_sub) {
+            max_index = i;
+        }
     }
+    swap(x[t], x[max_index]);
+    stack<int> st;
+
+    for (int i = t + 1; i < n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            if (x[j] < x[i]) {
+                swap(x[j], x[i]);
+            }
+        }
+	}
+	// for (int i = t + 1; i <= n; i++) {
+	// 	x[i] = st.top();
+    //     st.pop();
+	// }
+
 }
 int main () {
     #ifndef ONLINE_JUDGE
