defcdc8908b993bbcd237c0df22d9bd7cb542ec4
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2023-10-23 10:20:06 +0700

    revision

---
+++ listing/generation/genBinary/redo2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int n;
+int x[100];
+void display() {
+    for (int i = 1; i <= n; i++) {
+        cout << x[i] ;
+    }
+    cout << endl;
+}
+bool isFinal( ) {
+    for (int i = 1; i <= n; i++) {
+        if (x[i] == 0) {
+            return false;
+        }
+
+    }
+    return true;
+}
+void init() {
+    for (int i = 1; i <= n; i++) {
+        x[i] = 0;
+    }
+}
+void genNext( ){
+    int t = n;
+    while (x[t] == 1) {
+        t--;
+   }
+   x[t] = 1;
+    for (int i = t + 1; i <= n; i++){
+        x[i] = 0;
+    }
+}
+int main () {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    cin >> n;
+    init();
+    while (!isFinal()) {
+        display();
+        genNext();
+    }
+    display();
+}
---
+++ listing/generation/genHoanvi/redo3.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int n;
+int x[100];
+void display() {
+    for (int i = 1; i <= n; i++) {
+        cout << x[i] << " ";
+    }
+    cout << endl;
+}
+bool isFinal( ) {
+    for (int i = 1; i <= n; i++) {
+        if (x[i] != (n - i + 1)) {
+            return false;
+        }
+    }
+    return true;
+}
+void init() {
+    for (int i = 1; i <= n; i++) {
+        x[i] = i;
+    }
+}
+void genNext( ){
+    //98765
+    int t = n - 1;
+    while (x[t] > x[t + 1]) {
+        t--;
+    }
+    int k = n; 
+    while (x[t] > x[k]) {
+        k--;
+    }
+    swap(x[t], x[k]);
+    int left = t + 1;
+    int right = n;
+    while (left <= right) {
+        swap(x[left], x[right]);
+        left++;
+        right--;
+    }
+
+
+}
+int main () {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    cin >> n;
+    init();
+    while (!isFinal()) {
+        display();
+        genNext();
+    }
+    display();
+}
